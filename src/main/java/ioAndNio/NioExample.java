a4f4471b7b045b213a04082812a41a8839ab49bb
Enbuly <120157229@qq.com>
2019-03-21 10:03:08 +0800

    提交nio的ByteBuffer跟Channel

---
+++ src/main/java/ioAndNio/NioExample.java
@@ -0,0 +1,52 @@
+package ioAndNio;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author zhangzy
+ * @since 3-121
+ * NioExample
+ */
+public class NioExample {
+
+    private static void testChannel() {
+        try {
+            RandomAccessFile aFile = new RandomAccessFile(
+                    "E:\\IdeaProjects\\ioAndNio\\a.txt", "rw");
+            FileChannel inChannel = aFile.getChannel();
+
+            //create buffer with capacity of 48 bytes
+            ByteBuffer buf = ByteBuffer.allocate(48);
+
+            //read into buffer.
+            int bytesRead = inChannel.read(buf);
+
+            while (bytesRead != -1) {
+
+                System.out.println("Read " + bytesRead);
+
+                //make buffer ready for read
+                buf.flip();
+
+                while (buf.hasRemaining()) {
+                    System.out.print((char) buf.get());
+                }
+
+                //make buffer ready for writing
+                buf.clear();
+
+                bytesRead = inChannel.read(buf);
+            }
+            aFile.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        NioExample.testChannel();
+    }
+}
